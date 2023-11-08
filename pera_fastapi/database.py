372f81db9ef0f25d73c4ce7003035f81b8c0b66e
ionrusu114 <ionrusu114@gmail.com>
2023-11-08 17:55:53 +0200

    mysql problem solve 0.1

--- pera_fastapi/database.py
+++ pera_fastapi/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://peradmin:wQlG5unrYTLT9cTONHcg@localhost:3306/pera-api'
+URL_DATABASE = 'mysql+pymysql://peradmin:wQlG5unrYTLT9cTONHcg@62.72.21.180:3306/pera-api'
 
 engine = create_engine(URL_DATABASE)
 
